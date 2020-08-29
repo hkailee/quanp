20437fc2a325389ca3f5f2b61ac67d61be68f1d9
hkailee <leehongkai@gmail.com>
2020-08-30 00:13:01 +0800

    docs

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev41+g9e8cfe4.d20200829',
+    VERSION: '0.1.dev43+gf2fa77b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
